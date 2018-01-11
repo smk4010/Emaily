eed3ae7628291792f3b0715a80ed4d92e4aea0e1
Scott Kuchinski <scottkuchinski@gmail.com>
2018-01-11 11:00:40 -0500

    added cookie session package

--- services/passport.js
+++ services/passport.js
@@ -5,6 +5,16 @@ const keys = require('../config/keys');
 
 const User = mongoose.model('users');
 
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+      done(null, user);
+    });
+});
+
 passport.use(
   new GoogleStrategy(
     {
@@ -13,7 +23,19 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      new User({ googleId: profile.id }).save();
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            // users profile ID is in db already
+            done(null, existingUser);
+          } else {
+            // we do not have a user record, make one
+            new User({ googleId: profile.id })
+              .save()
+              .then(user => done(null, user));
+          }
+        })
+
     }
   )
 );
